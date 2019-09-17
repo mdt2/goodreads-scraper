2b868b998a1bd39a113c9733f01f74fe97551d49
Melissa Thompson <mthompson@heysparkbox.com>
2019-09-17 10:34:04 -0400

    feat: use puppeteer to scrape single goodreads page

--- index.js
+++ index.js
@@ -13,10 +13,34 @@ const success = chalk.keyword("green");
   try {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
+
     await page.goto(url);
-    await page.screenshot({ path: 'example.png' });
+    await page.waitForSelector('div.value > a[title]');
+
+    const results = await page.evaluate(() => {
+      // Vars have to live here for Chromium to interact with them
+      // Vars declared outside of page.evaluate() are inaccessible
+      let titles = document.querySelectorAll('div.value > a[title]');
+      let nextPage = document.querySelectorAll('.next_page');
+      let titleList = [];
+
+      for (let i = 0; i < titles.length; i++) {
+        titleList.push(titles[i].getAttribute('title'));
+      }
+
+      console.log(titleList);
+      return titleList;
+
+    });
+
     await browser.close();
-    console.log(success("Browser Closed"));
+
+    fs.writeFile('titles.json', JSON.stringify(results), (err) => {
+      if (err) throw err;
+      console.log('Saved!');
+    });
+
+    console.log("Brwoser Closed");
   } catch (err) {
     console.log(error(err));
     await browser.close();
