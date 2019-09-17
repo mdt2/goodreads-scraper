1d0a76995c541a16453cce42f2fbd90d2db5dfde
Melissa Thompson <mthompson@heysparkbox.com>
2019-09-17 09:47:27 -0400

    feat: use axios to scrape goodreads

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+const axios = require('axios');
+const fs = require('fs');
 const cheerio = require('cheerio');
-const $ = cheerio.load('<a title="My book title"></a><a title="one more of My book title"></a>');
-let titles = $('a[title]');
-const titleList = [];
 
-for (let i = 0; i < titles.length; i++) {
-  titleList.push(titles[i].attribs.title);
-  console.log(titleList);
+const url = 'https://www.goodreads.com/review/list/86630558-melissa-thompson?shelf=to-read';
+
+const getWebsiteContent = async (url) => {
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+
+    let titles = $('div.value > a[title]');
+    let titleList = [];
+    for (let i = 0; i < titles.length; i++) {
+      titleList.push(titles[i].attribs.title);
+    }
+    console.log(titleList);
+  } catch (error) {
+    console.error(error)
+  }
 }
+
+getWebsiteContent(url);
